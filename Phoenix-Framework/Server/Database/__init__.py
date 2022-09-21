a2b6daaf8b96a508143e37bc88ed73f5054d016f
screamz2k <luca.hennemann@outlook.de>
2022-09-21 16:24:39 +0200

    ss

--- Phoenix-Framework/Server/Database/__init__.py
+++ Phoenix-Framework/Server/Database/__init__.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists, create_database
 
 from Utils.config import load_config
 from Utils.ui import log
@@ -17,10 +16,7 @@ c = load_config()["database"]
 if c["type"] == "sqlite":
     engine = create_engine(f"sqlite:///{c['sqlite_location']}", connect_args={'check_same_thread': False})
 else:
-    conn_string = f"{c['type']}//{c['user']}:{c['pass']}@{c['host']}:{c['port']}/{'database'}"
-    if not database_exists(conn_string):
-        log(f"Database '{c['database']}' doesn't exist ({c['type']}). Creating it...", "info")
-        create_database(conn_string)
+    conn_string = f"{c['type']}://{c['user']}:{c['pass']}@{c['host']}:{c['port']}/{c['database']}"
     engine = create_engine(conn_string)
 
     
