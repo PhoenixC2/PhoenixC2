e9238371d6de04bdeb48b3d8d7f6e321839afbfa
screamz2k <luca.hennemann@outlook.de>
2022-09-21 20:58:35 +0200

    ss

--- Phoenix-Framework/listener.py
+++ Phoenix-Framework/listener.py
@@ -1,23 +1,37 @@
-import threading
-import time
 from multiprocessing import Process
+from threading import Thread
 
 from flask import Flask, request
 
 
-class Listener(Flask):
+class Listener():
     """The Reverse Http Listener Class"""
-    api = Flask()
+    api = Flask(__name__)
+
     def __init__(self):
         super().__init__()
         self.address = "0.0.0.0"
         self.port = 9999
         self.handlers: list = []
-        self.listener_thread: threading.Thread
-        self.refresher_thread: threading.Thread
+        self.listener_process: Process
+        self.create_api()
 
-    @api.route("/connect")
-    def _connect(self):
-        request.re
+    def create_api(self):
+        self.api = Flask(__name__)
 
-    def listen()
+        @self.api.route("/connect", methods=["POST"])
+        def connect():
+            data = request.get_json()
+
+    def start(self):
+        self.listener_process = Process(target=self.api.run)
+        Thread(target=self.listener_process.start).start()
+
+    def stop(self):
+        self.listener_process.kill()
+
+
+listener = Listener()
+listener.start()
+input()
+listener.stop()
