3f150ab903e4e1ef96dd8805d69a181415b5bd58
screamz2k <luca.hennemann@outlook.de>
2023-03-10 19:25:58 +0100

    added direct execution

---
+++ phoenixc2/__main__.py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1 or sys.argv[1] not in ["server", "client"]:
+        print("Usage: python -m phoenixc2 [server|client]")
+        sys.exit(1)
+
+    if sys.argv[1] == "server":
+        from .server.__main__ import main
+        sys.argv.pop(1)
+        sys.exit(main())
+    elif sys.argv[1] == "client":
+        from .client.__main__ import main
+        sys.argv.pop(1)
+        sys.exit(main())
+
+    
